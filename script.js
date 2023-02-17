2274e400102bea7be86d23434809ca781ad59b4e
RezonHamiti <110455891+RezonHamiti@users.noreply.github.com>
2023-02-17 19:28:35 +0100

    update deleteBtn to dblclick, update deleteInput() function

--- script.js
+++ script.js
@@ -4,52 +4,45 @@ const ulElement = document.querySelector("#ul-element");
 const deleteBtn = document.querySelector("#input-delete");
 let myLeads = [];
 
+const leadsFromLocal = JSON.parse(localStorage.getItem("myLeads"));
 
 inputBtn.addEventListener("click", saveInput);
-deleteBtn.addEventListener("click", deleteInput);
+deleteBtn.addEventListener("dblclick", deleteInput);
 
 function deleteInput() {
   localStorage.clear();
-  location.reload();
+  myLeads = [];
+  ulElement.innerHTML = null;
 }
-
-
 
 // get leads from the localStorage
-let leadsFromLocal = JSON.parse( localStorage.getItem("myLeads"));
-if(leadsFromLocal) {
+if (leadsFromLocal) {
   myLeads = leadsFromLocal;
-  renderLeads();
+  render(myLeads);
 }
 
-
-
-
-
-
-
-function saveInput() {
-  myLeads.push(inputElement.value);
-  // clear the input field onclick
-  inputElement.value = "";
-  localStorage.setItem("myLeads", JSON.stringify(myLeads));
-  renderLeads();
-  // log the local storage
-  console.log(localStorage.getItem("myLeads"));
-}
-
-function renderLeads() {
+function render(leads) {
   let listItems = "";
-  for (let i = 0; i < myLeads.length; i++) {
+  for (let i = 0; i < leads.length; i++) {
     // place input in a tag so it can be clicked, open new browser with target='_blank'
     listItems += `
         <li> 
-          <a target='_blank' href= '${myLeads[i]}'>
-            ${myLeads[i]}
+          <a target='_blank' href= '${leads[i]}'>
+            ${leads[i]}
           </a>
         </li>
     `;
   }
   // render the myLeads items inside the ul
   ulElement.innerHTML = listItems;
+}
+
+function saveInput() {
+  myLeads.push(inputElement.value);
+  // clear the input field onclick
+  inputElement.value = "";
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  render(myLeads);
+  // log the local storage
+  console.log(localStorage.getItem("myLeads"));
 }
